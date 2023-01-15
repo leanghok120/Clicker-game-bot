e2db829e3988e792972e297633392bfeda85bb67
Leanghok120 <leanghokoeng5@gmail.com>
2023-01-15 09:02:08 +0700

    Improve the cord gathering system

--- mouse_pos_gathering.py
+++ mouse_pos_gathering.py
@@ -3,17 +3,17 @@ import time
 import customtkinter
 import tkinter
 
-root = customtkinter.CTk()
-root.geometry("500x450")
-root.title("Coordinates Gathering")
-frame_1 = customtkinter.CTkFrame(master=root)
-frame_1.pack(pady=20, padx=60, fill="both", expand=True)
-label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Coordinates")
-label_1.pack(pady=10, padx=10)
-
 
 def cords():
     print("Coordinates Gathering: Enabled")
+    root = customtkinter.CTk()
+    root.geometry("500x450")
+    root.title("Coordinates Gathering")
+    frame_1 = customtkinter.CTkFrame(master=root)
+    frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+    label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Coordinates")
+    label_1.pack(pady=10, padx=10)
+
     time.sleep(3)
     for i in range(0, 11):
         label_2 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text=pyautogui.position())
@@ -21,3 +21,6 @@ def cords():
         time.sleep(0.5)
     root.mainloop()
     print("Coordinates Gathering: Disabled")
+
+
+cords()
